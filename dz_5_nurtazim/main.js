ce1e3998e619a79a94847e046f39633639518e28
nurtazim <bakytovnurtazim@gmail.com>
2021-11-26 22:13:27 +0600

    коментарии не разблокировал

--- dz_5_nurtazim/main.js
+++ dz_5_nurtazim/main.js
@@ -133,31 +133,31 @@
 //
 // countChar("FNFDJNJSN")
 
+//
+// let num = 0
+
+// function getValue(e){
+//     e.target.classList.toggle("active")
+//     num = num + 1
+//     document.getElementById("min").innerHTML = num
+//
+// }
+// function getValuemin(e){
+//      e.target.classList.toggle("active")
+//     num = num - 1
+//     document.getElementById("min").innerHTML = num
+// }
 
 let num = 0
 
-function getValue(e){
+const getValue = (e) => {
     e.target.classList.toggle("active")
     num = num + 1
-    document.getElementById("min").innerHTML = num
-
+    document.getElementById("min").innerHTML=num
 }
-function getValuemin(e){
-     e.target.classList.toggle("active")
+
+const getValuemin = (e) => {
+    e.target.classList.toggle("active")
     num = num - 1
-    document.getElementById("min").innerHTML = num
+    document.getElementById("min").innerHTML=num
 }
-
-// let num = 0
-//
-// const getValue = (e) => {
-//     e.target.classList.toggle("active")
-//     num = num + 1
-//     document.getElementById("min").innerHTML=num
-// }
-//
-// const getValuemin = (e) => {
-//     e.target.classList.toggle("active")
-//     num = num - 1
-//     document.getElementById("min").innerHTML=num
-// }
